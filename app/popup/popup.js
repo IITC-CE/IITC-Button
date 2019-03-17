6ce9983a0377df5ece154736556e585644dcd18c
Alexander Danilov <modos189@protonmail.com>
2019-03-17 23:11:39 +0300

    The initial version of popup menu IITC Button extension

--- app/popup/popup.js
+++ app/popup/popup.js
@@ -1,27 +1,61 @@
-'use strict';
+var categories = [
+    {name: 'Portal Info', description: 'Enhanced information on the selected portal'},
+    {name: 'Info', description: 'Display additional information'},
+    {name: 'Keys', description: 'Manual key management'},
+    {name: 'Controls', description: 'Map controls/widgets'},
+    {name: 'Highlighter', description: 'Portal highlighters'},
+    {name: 'Layer', description: 'Additional map layers'},
+    {name: 'Map Tiles', description: 'Alternative map layers'},
+    {name: 'Tweaks', description: 'Adjust IITC settings'},
+    {name: 'Misc', description: 'Unclassified plugins'},
+    {name: 'Obsolete', description: 'Plugins that are no longer recommended, due to being superceded by others or similar'}
+];
 
-var buildListView = (list) => {
-  let result ='';
-  
-  list.reduce((prev, current, index, arr) => {
-    let [key, value] = current;
-    let localStoragePluginValue = localStorage[`plugin/${key}`];
-    // console.log(key, value, localStorage['plugin/'+key])
-    if (localStoragePluginValue !== undefined) value = localStoragePluginValue === 'true'; 
-    let res = `<li class="list-group-item">${key}
-            <div class="pull-right">
-                <input type="checkbox" data-plugin-name="${key}" name="plugin-checkbox" ${value ? 'checked="checked"': ''} >
-            </div>
-          </li>`;
-    result += res;
-  }, result);
+var ractive = new Ractive({
+  target: '#target-ractive',
+  template: '#template-ractive',
+  data: {categories: categories}
+});
 
-  $('.list-group').html('').append(result);
-}; 
+ractive.on('openIITC', function (event) {
+  event.original.preventDefault();
+  chrome.tabs.create({
+      url: 'https://intel.ingress.com/intel',
+      pinned: true
+  });
+});
 
-pluginHelpers.getPluginList(null, buildListView);
-pluginHelpers.setupButtons();
-pluginHelpers.listenFormChanges();
+
+
+
+
+
+
+
+// 'use strict';
+//
+// var buildListView = (list) => {
+//   let result ='';
+//
+//   list.reduce((prev, current, index, arr) => {
+//     let [key, value] = current;
+//     let localStoragePluginValue = localStorage[`plugin/${key}`];
+//     // console.log(key, value, localStorage['plugin/'+key])
+//     if (localStoragePluginValue !== undefined) value = localStoragePluginValue === 'true';
+//     let res = `<li class="list-group-item">${key}
+//             <div class="pull-right">
+//                 <input type="checkbox" data-plugin-name="${key}" name="plugin-checkbox" ${value ? 'checked="checked"': ''} >
+//             </div>
+//           </li>`;
+//     result += res;
+//   }, result);
+//
+//   $('.list-group').html('').append(result);
+// };
+//
+// pluginHelpers.getPluginList(null, buildListView);
+// pluginHelpers.setupButtons();
+// pluginHelpers.listenFormChanges();
 /*
 Example 
  */
