d8fa0eb470d3b51773f8cdeddbff42e85766d62f
Alexander Danilov <modos189@protonmail.com>
2019-04-14 14:03:51 +0300

    Fix for ingressmosaik plugin. This plugin uses Tampermonkey-specific feature to access site’s context instead of in the Greasemonkey/Extension/etc. context. #2

--- scripts/pre.js
+++ scripts/pre.js
@@ -1,3 +1,9 @@
+// Fix for ingressmosaik plugin.
+// This plugin uses Tampermonkey-specific feature to access site’s context
+// instead of in the Greasemonkey/Extension/etc. context.
+window.plugin = {};
+window.plugin.missions = true;
+
 // A simple solution for Firefox
 if (window.wrappedJSObject) {
   window.PLAYER = window.wrappedJSObject.PLAYER;
