740e7b33764a0f7a64a83ddb19065f9b29b88d37
Alexander Danilov <git@modos189.ru>
2020-09-09 00:02:58 +0300

    As default address for local server, use localhost instead of 127.0.0.1 to support IPv6

--- scripts/defaults.js
+++ scripts/defaults.js
@@ -4,5 +4,5 @@ let channel = 'release';
 let network_host = {
   'release': "https://iitc.app/build/release",
   'test': "https://iitc.app/build/test",
-  'local': "http://127.0.0.1:8000"
+  'local': "http://localhost:8000"
 }
