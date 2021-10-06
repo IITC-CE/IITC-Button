8b9bfa0ae2be344b6bc0a1df9322cc2d43e473a1
lucka-me <i@lucka.moe>
2021-10-06 11:21:28 +0800

    Add state indicator

--- safari/Shared/ContentView.swift
+++ safari/Shared/ContentView.swift
@@ -6,11 +6,62 @@
 //
 
 import SwiftUI
+import SafariServices
 
 struct ContentView: View {
+    
+    private enum ExtensionStatus {
+        case unknown
+        case disabled
+        case enabled
+    }
+    
+    static private let extensionId = "dev.lucka.IITC-Button.Safari-Extension"
+    
+    @Environment(\.scenePhase) private var scenePhase : ScenePhase
+    @State private var extensionStatus = ExtensionStatus.unknown
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Text("Safari Extension")
+            Spacer()
+            Text(statusText)
+                .foregroundColor(statusColor)
+        }
+        .padding()
+        .onChange(of: scenePhase) { phase in
+            if phase == .active {
+                updateExtensionStatus()
+            }
+        }
+    }
+    
+    private var statusText: LocalizedStringKey {
+        switch extensionStatus {
+            case .unknown: return "Unknown"
+            case .disabled: return "Disabled"
+            case .enabled: return "Enabled"
+        }
+    }
+    
+    private var statusColor: Color {
+        switch extensionStatus {
+            case .unknown: return .orange
+            case .disabled: return .red
+            case .enabled: return .green
+        }
+    }
+    
+    private func updateExtensionStatus() {
+        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: Self.extensionId) { state, error in
+            guard let solidState = state, error == nil else {
+                extensionStatus = .unknown
+                return
+            }
+            DispatchQueue.main.async {
+                extensionStatus = solidState.isEnabled ? .enabled : .disabled
+            }
+        }
     }
 }
 
