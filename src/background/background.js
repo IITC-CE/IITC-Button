c0bf172e7a08e027952ac2a943c50839f5fc7a4c
Alexander Danilov <git@modos189.ru>
2024-04-21 17:08:02 +0300

    Error catching fixes

--- src/background/background.js
+++ src/background/background.js
@@ -24,28 +24,22 @@ import {
 const manager = new Manager({
   storage: browser.storage.local,
   message: (message, args) => {
-    try {
-      browser.runtime
-        .sendMessage({
-          type: "showMessage",
-          message: _(message, args),
-        })
-        .then();
-    } catch {
-      // If popup is closed, message goes nowhere and an error occurs. Ignore.
-    }
+    browser.runtime
+      .sendMessage({
+        type: "showMessage",
+        message: _(message, args),
+      })
+      .then()
+      .catch(() => {}); // If popup is closed, message goes nowhere and an error occurs. Ignore.
   },
   progressbar: (is_show) => {
-    try {
-      browser.runtime
-        .sendMessage({
-          type: "showProgressbar",
-          value: is_show,
-        })
-        .then();
-    } catch {
-      // If popup is closed, message goes nowhere and an error occurs. Ignore.
-    }
+    browser.runtime
+      .sendMessage({
+        type: "showProgressbar",
+        value: is_show,
+      })
+      .then()
+      .catch(() => {}); // If popup is closed, message goes nowhere and an error occurs. Ignore.
   },
   inject_plugin: async (plugin) => {
     if (IS_USERSCRIPTS_API) return;
@@ -103,67 +97,52 @@ browser.runtime.onMessage.addListener(async (request) => {
     case "addUserScripts":
       // TODO: The onMessage method should be able to return a value, but does not do so because of a bug.
       //  More info: https://github.com/mozilla/webextension-polyfill/issues/172
-      try {
-        browser.runtime
-          .sendMessage({
-            type: "resolveAddUserScripts",
-            scripts: await manager.addUserScripts(request.scripts),
-          })
-          .then();
-      } catch {
-        // If tab is closed, message goes nowhere and an error occurs. Ignore.
-      }
+      browser.runtime
+        .sendMessage({
+          type: "resolveAddUserScripts",
+          scripts: await manager.addUserScripts(request.scripts),
+        })
+        .then()
+        .catch(() => {}); // If tab is closed, message goes nowhere and an error occurs. Ignore.
       break;
     case "getPluginInfo":
-      try {
-        browser.runtime
-          .sendMessage({
-            type: "resolveGetPluginInfo",
-            info: await manager.getPluginInfo(request.uid),
-          })
-          .then();
-      } catch {
-        // If tab is closed, message goes nowhere and an error occurs. Ignore.
-      }
+      browser.runtime
+        .sendMessage({
+          type: "resolveGetPluginInfo",
+          info: await manager.getPluginInfo(request.uid),
+        })
+        .then()
+        .catch(() => {}); // If tab is closed, message goes nowhere and an error occurs. Ignore.
       break;
     case "getBackupData":
-      try {
-        browser.runtime
-          .sendMessage({
-            type: "resolveGetBackupData",
-            data: await manager.getBackupData(request.params),
-          })
-          .then();
-      } catch {
-        // If tab is closed, message goes nowhere and an error occurs. Ignore.
-      }
+      browser.runtime
+        .sendMessage({
+          type: "resolveGetBackupData",
+          data: await manager.getBackupData(request.params),
+        })
+        .then()
+        .catch(() => {}); // If tab is closed, message goes nowhere and an error occurs. Ignore.
       break;
     case "setBackupData":
-      try {
-        browser.runtime
-          .sendMessage({
-            type: "resolveSetBackupData",
-            data: await manager.setBackupData(
-              request.params,
-              request.backup_data
-            ),
-          })
-          .then();
-      } catch {
-        // If tab is closed, message goes nowhere and an error occurs. Ignore.
-      }
+      browser.runtime
+        .sendMessage({
+          type: "resolveSetBackupData",
+          data: await manager.setBackupData(
+            request.params,
+            request.backup_data
+          ),
+        })
+        .then()
+        .catch(() => {}); // If tab is closed, message goes nowhere and an error occurs. Ignore.
       break;
     case "checkUserScriptsApiAvailable":
-      try {
-        browser.runtime
-          .sendMessage({
-            type: "resolveCheckUserScriptsApiAvailable",
-            data: is_userscripts_api_available(),
-          })
-          .then();
-      } catch {
-        // If tab is closed, message goes nowhere and an error occurs. Ignore.
-      }
+      browser.runtime
+        .sendMessage({
+          type: "resolveCheckUserScriptsApiAvailable",
+          data: is_userscripts_api_available(),
+        })
+        .then()
+        .catch(() => {}); // If tab is closed, message goes nowhere and an error occurs. Ignore.
       break;
     case "setCustomChannelUrl":
       await manager.setCustomChannelUrl(request.value);
