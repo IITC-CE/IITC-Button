c26efe8368004a65e7fc402f3168cc6dae83cfa8
Alexander Danilov <git@modos189.ru>
2024-06-20 18:40:51 +0500

    Added timestamp for beta versions

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,17 @@ const manifest_transformer = (manifest) => {
       "assets/icons/96/icon-beta.png";
     manifest.icons["48"] = "assets/icons/48/icon-beta.png";
     manifest.icons["128"] = "assets/icons/128/icon-beta.png";
+
+    let currentDate = new Date();
+    let year = currentDate.getFullYear();
+    let month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    let day = String(currentDate.getDate()).padStart(2, "0");
+    let hours = String(currentDate.getHours()).padStart(2, "0");
+    let minutes = String(currentDate.getMinutes()).padStart(2, "0");
+    let seconds = String(currentDate.getSeconds()).padStart(2, "0");
+
+    let formattedDate = `${year}${month}${day}.${hours}${minutes}${seconds}`;
+    manifest.version = `${manifest.version}.${formattedDate}`;
   }
 
   if (manifest_version === "2") {
