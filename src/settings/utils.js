1fdaea0b8ebfd583593c64d74e247f8b471b204a
Alexander Danilov <git@modos189.ru>
2024-09-19 21:32:20 +0500

    Added ability to restore localstorage from json file for debugging purposes

---
+++ src/settings/utils.js
@@ -0,0 +1,17 @@
+//@license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3
+
+export const readUploadedFileAsText = (inputFile) => {
+  const temporaryFileReader = new FileReader();
+
+  return new Promise((resolve, reject) => {
+    temporaryFileReader.onerror = () => {
+      temporaryFileReader.abort();
+      reject(new DOMException("Problem parsing input file."));
+    };
+
+    temporaryFileReader.onload = () => {
+      resolve(temporaryFileReader.result);
+    };
+    temporaryFileReader.readAsText(inputFile);
+  });
+};
