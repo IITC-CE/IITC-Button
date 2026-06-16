4d5b870d7e9beb36cc17ec2c938487a212b8a28c
Alexander Danilov <git@modos189.ru>
2026-06-16 21:14:33 +0500

    chore: set up TypeScript infrastructure for JS -> TS migration

---
+++ src/types/scored-fuzzysearch.d.ts
@@ -0,0 +1,22 @@
+declare module "scored-fuzzysearch" {
+  interface SearcherOptions {
+    scoreIncreasePerSequencialMatchedLetter?: number;
+    initialScoreDecreasePerLetter?: number;
+    scoreDecreasePerLetterBeforeMatch?: number;
+    scoreDecreasePerLetterAfterMatch?: number;
+  }
+
+  interface MatchIndex {
+    start: number;
+    length: number;
+  }
+
+  type Searcher = (needle: string, haystack: string) => number;
+
+  export function createSearcher(options: SearcherOptions): Searcher;
+  export function fuzzysearch(needle: string, haystack: string): number;
+  export function findMatchIndices(
+    needle: string,
+    haystack: string,
+  ): MatchIndex[] | 0;
+}
